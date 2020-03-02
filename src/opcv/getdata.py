c20e67de4cc308393a89f63fc1af17223570481f
Fedo Zhu <fedozhu@Fedos-MacBook-Pro.local>
2020-03-01 19:32:28 -0500

    change to allow optional input

--- src/opcv/getdata.py
+++ src/opcv/getdata.py
@@ -1,21 +1,21 @@
-
-from sodapy import Socrata
-import json
-
-def get_data(page_size, num_pages, output):
+def get_data(pag_size, num_pages, output_file):
     client = Socrata("data.cityofnewyork.us", "io7xTnlVGyUEHLw5g4QflUqph")
     offset = 0
     res = []
-    f = open("data.json", "w")
+    # If num_pages is not provided, calculate the num_pages so that we can read the entire content.
+    if num_pages is None:
+        total_records = int(client.get("nc67-uf89", select="COUNT(*)")[0]["COUNT"])
+        num_pages = total_records // page_size + 1
+    if output_file is not None:
+    	f = open(output_file, "w")
     for i in range(num_pages):
         one_page = client.get("nc67-uf89", limit=page_size, offset=offset)
-        for item in one_page:
-            res.append(item)
-        offset += page_size
-    if output:
-        print(res)
-    else: 
+        if output_file is None:
+            print(one_page)
+        else:
+            for item in one_page:
+                res.append(item)
+                offset += page_size
+    if output_file is not None:
         json.dump(res, f)
-
-
-
+        f.close()
