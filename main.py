0cf52e7c735647bd0d4b1fcf0bea8639c07e3ddf
Fedo Zhu <fedozhu@Fedos-MacBook-Pro.local>
2020-03-01 19:36:28 -0500

    change to allow optional input

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
+import argparse
 import sys
 from src.opcv.getdata import get_data
 
 if __name__ == '__main__':
-	args = sys.argv[1:]
-	page_size = int(args[0])
-	try:
-		num_pages = int(args[1])
-	except:
-		num_pages = 1
-	try:
-		output = args[2]
-	except:
-		output = None
+    ## Take arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--page_size", help="how many records to request from the API per call", type=int)
+    parser.add_argument("--num_pages", help="how many calls we make", type=int)
+    parser.add_argument("--output", help=" if not provided, your script should print results to stdout; if provided, your script should write the data to the file", type=str)
+    
+    args = parser.parse_args()
+    page_size = args.page_size
+    num_pages = args.num_pages
+    output_file = args.output
 
-	get_data(page_size,num_pages)
+	get_data(page_size,num_pages,output)
 	
