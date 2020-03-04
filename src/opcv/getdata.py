480c9599852b5c824decb64f63a2c9b6fbb3d567
Fedo Zhu <fedozhu@Fedos-MacBook-Pro.local>
2020-03-04 11:24:48 -0500

    add Line Feed to output file

--- src/opcv/getdata.py
+++ src/opcv/getdata.py
@@ -9,16 +9,24 @@ def get_data(app_key,page_size, num_pages, output_file):
     if num_pages is None:
         total_records = int(client.get("nc67-uf89", select="COUNT(*)")[0]["COUNT"])
         num_pages = total_records // page_size + 1
+    #load Json_file is output is provided
     if output_file is not None:
-    	f = open(output_file, "w")
+    	#f = open(output_file, "w")
+        f = open(output_file, 'a')
+    # Get records        
     for i in range(num_pages):
         one_page = client.get("nc67-uf89", limit=page_size, offset=offset)
         if output_file is None:
             print(one_page)
         else:
             for item in one_page:
-                res.append(item)
-                offset += page_size
+                f.write(json.dumps(item) + '\n')
+        offset += page_size
+
     if output_file is not None:
-        json.dump(res, f)
         f.close()
+    
+    # data = json.loads(f)
+    # print(data)
+
+
